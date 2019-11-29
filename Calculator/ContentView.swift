6f169249feb85bd1f28871cd195b24875c8a870b
Matthiti <mj.roelink@outlook.com>
2019-11-29 14:54:17 +0100

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Matthijs Roelink on 29/11/2019.
+//  Copyright © 2019 Matthijs Roelink. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
